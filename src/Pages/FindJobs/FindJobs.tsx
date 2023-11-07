407455e57f49d7734eadca76b58cca1fc3d88a88
zealnex-guest <saik64757@gmail.com>
2023-11-08 03:53:25 +0530

    findJobs-Page-Updated

--- src/Pages/FindJobs/FindJobs.tsx
+++ src/Pages/FindJobs/FindJobs.tsx
@@ -4,15 +4,19 @@ import styles from "./FindJobs.module.css";
 
 function FindJobs() {
   return (
-    <div className={styles.findjobsWrapper}>
-      <SearchSection>
-        <h1>Find your</h1>
-        <p>Find your next carrer at companies like HubSpot,Nike and Dropbox</p>
-      </SearchSection>
+    <>
+      <div className={styles.findjobsWrapper}>
+        <SearchSection>
+          <h1>Find your</h1>
+          <p>
+            Find your next carrer at companies like HubSpot,Nike and Dropbox
+          </p>
+        </SearchSection>
+      </div>
       <section className={styles.allJobsWrapper}>
         <FilterDropdown />
       </section>
-    </div>
+    </>
   );
 }
 
