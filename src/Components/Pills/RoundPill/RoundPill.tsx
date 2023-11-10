e98217732d765f293bfed2dd1303c84dc8adb46a
zealnex-guest <saik64757@gmail.com>
2023-11-11 00:20:59 +0530

    Browser Companies Landing Page done

--- src/Components/Pills/RoundPill/RoundPill.tsx
+++ src/Components/Pills/RoundPill/RoundPill.tsx
@@ -6,15 +6,17 @@ interface roundPill {
   color: string;
   bgColor: string;
   children?: React.ReactNode;
+  fontSize?: string;
 }
 
-function RoundPill({ color, bgColor, children }: roundPill) {
+function RoundPill({ color, bgColor, children, fontSize }: roundPill) {
   return (
     <div
       className={clsx(styles.roundPillWrapper)}
       style={{
         color: color,
         backgroundColor: bgColor,
+        fontSize: fontSize,
       }}
     >
       {children}
--- src/Pages/BrowserCompanies/BrowserCompanies.tsx
+++ src/Pages/BrowserCompanies/BrowserCompanies.tsx
@@ -2,9 +2,29 @@ import JobsCardSquare from "../../Components/JobscardSqr/JobscardSqr";
 import SearchSection from "../../Components/SearchSection/SearchSection";
 import styles from "./BrowserCompanies.module.css";
 import DesignLogo from "../../Assets/Icons/pencil-paintbrush.png";
+import DesignLogoColored from "../../Assets/Icons/pencil-paintbrush colored.png";
 import { categories } from "../../Data/Options";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./index.css";
+import { useState } from "react";
+import RoundPill from "../../Components/Pills/RoundPill/RoundPill";
 
 function BrowserCompanies() {
+  const [iscategorySelected, setiscategorySelected] = useState(false);
+
+  let arr1 = Array.from({ length: 8 }, () => 0);
+
+  // Slider Settings
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
+
   let newArr = Array.from({ length: 6 }, () => 0);
   return (
     <div className={styles.browserCompaniesWrapper}>
@@ -25,12 +45,48 @@ function BrowserCompanies() {
           ))}
         </div>
       </div>
-      {(categories as unknown as any[]).map((ele) => (
-        <div className={styles.catagorycarasoul}>
-          <img src={DesignLogo} alt="DesignLogo" />
-          <h1>{ele.name}</h1>
+      <div className={styles.CompaniesByCatagory}>
+        <h1>Companies By Category</h1>
+        <Slider {...settings}>
+          {(categories as unknown as any[]).map((ele) => (
+            <div className={styles.catagorycarasoul}>
+              <img src={DesignLogo} alt="DesignLogo" />
+              <h1>{ele.name}</h1>
+            </div>
+          ))}
+        </Slider>
+      </div>
+      <div className={styles.companyResultsWrapper}>
+        <div>
+          <img
+            src={DesignLogoColored}
+            alt="DesignLogo"
+            width={"30rem"}
+            height={"30rem"}
+          />
+
+          <p>24 Results</p>
         </div>
-      ))}
+        <div className={styles.companiesCardWrapper}>
+          {arr1.map(() => (
+            <div>
+              <div className={styles.companiesCard}>
+                <img
+                  src={DesignLogoColored}
+                  alt=""
+                  width={"50rem"}
+                  height={"50rem"}
+                />
+                <h2>PentaGram</h2>
+                <RoundPill bgColor="#4540de16" color="#4640de" fontSize="2rem">
+                  3 Jobs
+                </RoundPill>
+              </div>
+            </div>
+          ))}
+        </div>
+        <p>View more Design companies &#129122;</p>
+      </div>
     </div>
   );
 }
